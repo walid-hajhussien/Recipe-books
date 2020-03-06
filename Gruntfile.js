2aec399659f03b2cdc96152101bdb26e340893b0
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-03-06 11:50:19 +0100

    (add) bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
       gitHub: {
         command: [
           "git add .",
-          'git commit -m "(add) grunt file"',
+          'git commit -m "(add) bootstrap"',
           "git push origin master"
         ].join("&&")
       }
