c1ff99a2d712d28ff1f8a911abd09c6c080dc8a8
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-03-07 13:35:24 +0100

    (add) IngredientModel

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
       gitHub: {
         command: [
           "git add .",
-          'git commit -m "(refactor) recipe list component "',
+          'git commit -m "(add) IngredientModel "',
           "git push origin master"
         ].join("&&")
       }
