9ecf7dbab2dfa85b522ddd6ee93bdb52c639d9ad
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-04-24 22:08:37 +0100

    (add) auth reducer &&git push origin master

--- src/app/store/authStore/auth.reducer.ts
+++ src/app/store/authStore/auth.reducer.ts
@@ -1,6 +1,21 @@
 import {AuthStateInterface} from '../../interfaces/store/auth-state-interface';
 import {authState} from './auth.state';
+import {AuthActionTypes, TypeAuthActions} from './auth.action';
 
-export function authReducer(state: AuthStateInterface = authState, action) {
-  return state;
+export function authReducer(state: AuthStateInterface = authState, action: TypeAuthActions) {
+  switch (action.type) {
+    case AuthActionTypes.LOGIN:
+      return {
+        ...state,
+        user: action.payLoad
+      };
+    case AuthActionTypes.LOGOUT:
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
+
 }
