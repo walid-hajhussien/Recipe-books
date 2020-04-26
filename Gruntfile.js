d7826e60b7611b9f551c8f10b7202ee74e5d71e9
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-04-26 13:04:44 +0100

    (deprecated) shopping list service &&git push origin master

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
       gitHub: {
         command: [
           "git add .",
-          'git commit -m "(refactor) shopping list service ',
+          'git commit -m "(deprecated) shopping list service ',
           "git push origin master"
         ].join("&&")
       }
--- src/app/services/shoppingList/shopping-list.service.ts
+++ src/app/services/shoppingList/shopping-list.service.ts
@@ -1,14 +1,11 @@
 import {Injectable} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {AppStateInterface} from '../../interfaces/store/app-state-interface';
-
-
+// this service has been deprecated by using NgRx
 @Injectable({
   providedIn: 'root'
 })
 export class SShoppingListService {
 
-  constructor(private store: Store<AppStateInterface>) {
+  constructor() {
   }
 
 }
