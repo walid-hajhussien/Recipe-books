0ca3df39335b89719a69daa8ceeddd52cf4aa429
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-05-03 12:01:22 +0100

    add nestJs server forangular universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AppStateInterface} from './interfaces/store/app-state-interface';
 import {AutoLoginRequestAction} from './store/authStore/auth.action';
 import {UsersIdleService} from './services/usersIdle/users-idle.service';
+import {isPlatformBrowser} from '@angular/common';
 
 
 @Component({
@@ -13,12 +14,19 @@ import {UsersIdleService} from './services/usersIdle/users-idle.service';
 export class AppComponent implements OnInit {
   title = 'Recipe-books';
 
-  constructor(private store: Store<AppStateInterface>, private usersIdleService: UsersIdleService) {
+  constructor(
+    private store: Store<AppStateInterface>,
+    private usersIdleService: UsersIdleService,
+    @Inject(PLATFORM_ID) private platformId) {
 
   }
 
   ngOnInit(): void {
-    this.store.dispatch(new AutoLoginRequestAction());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AutoLoginRequestAction());
+    }
+    const platformName = isPlatformBrowser(this.platformId) ? 'Browser' : 'server';
+    console.log('Platform Name : ' + platformName);
   }
 
 
